3207256530a2e54716177cf4966ec4c5de75c476
HYEMBURGERWORLD <burgerhyem@gmail.com>
2023-05-25 23:06:58 +0900

    🚩 Feat check start painting

--- script/canvas.js
+++ script/canvas.js
@@ -27,9 +27,41 @@ const brushColor = document.getElementById('color').value;
 ctx.strokeStyle = brushColor;
 
 /** ************************* */
+/**         variable          */
+/** ************************* */
+
+let isDrawing = false;
+
+/** ************************* */
 /**         function          */
 /** ************************* */
+// * Drawing now?
+function startDrawing() {
+  isDrawing = true;
+}
+
+function stopDrawing() {
+  isDrawing = false;
+}
+
+function moveBrush(e) {
+  if (isDrawing) {
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    return;
+  }
+
+  // just move
+  ctx.beginPath();
+  ctx.moveTo(e.offsetX, e.offsetY);
+}
 
 /** ************************* */
 /**       event listner       */
 /** ************************* */
+
+// * Drawing now?
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mouseup', stopDrawing);
+canvas.addEventListener('mouseleave', stopDrawing);
+canvas.addEventListener('mousemove', moveBrush);

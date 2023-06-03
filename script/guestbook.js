e1a1c217d5619ce3b6cf714ac109028c2bd1487b
HYEMBURGERWORLD <burgerhyem@gmail.com>
2023-06-03 21:06:15 +0900

    🚩 Feat del, check pw

--- script/guestbook.js
+++ script/guestbook.js
@@ -1,24 +1,24 @@
 'use strict';
 
-const record = JSON.parse(localStorage.getItem('record'));
+let record = JSON.parse(localStorage.getItem('record'));
 
 function createHtmlElement(record) {
   const li = document.createElement('li');
   const img = document.createElement('img');
-  const button = document.createElement('button');
+  const span = document.createElement('span');
   const records = document.getElementById('records');
 
   img.id = record.id;
   img.src = record.img;
-  button.innerText = 'X';
-  button.id = 'del';
+  span.innerText = 'X';
+  span.id = 'del';
 
   li.classList.add('record');
   img.classList.add('record-img');
-  button.classList.add('record-btn');
+  span.classList.add('del-btn');
 
-  li.appendChild(button);
   li.appendChild(img);
+  li.appendChild(span);
   records.appendChild(li);
 }
 
@@ -28,4 +28,38 @@ function seeRecord() {
   }
 }
 
+function delRecord(e) {
+  const id = e.target.previousElementSibling.id;
+  const li = e.target.parentElement;
+  const pw = record.filter((img) => img.id === Number(id))[0].pw;
+
+  // 비밀번호 확인
+  checkPw(pw, id, li);
+}
+
+function checkPw(pw, id, li) {
+  // 모달창 활용
+  onModal();
+  const confirmPw = document.getElementById('confirmPw');
+  confirmPw.addEventListener('click', () => {
+    const inputPw = document.getElementById('pw').value;
+    if (inputPw === pw) {
+      li.remove();
+      record = record.filter((img) => img.id !== Number(id));
+      delStorage();
+    } else {
+      console.log('xxx');
+    }
+    offModal();
+  });
+}
+
+function delStorage() {
+  localStorage.setItem('record', JSON.stringify(record));
+}
+
 seeRecord();
+const delBtn = Array.from(document.getElementsByClassName('del-btn'));
+delBtn.forEach((btn) => {
+  btn.addEventListener('click', delRecord);
+});
